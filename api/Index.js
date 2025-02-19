895b55786f66d5aac27a2862b3cacf08dda282b7
riya-3110 <mriya0467@gmail.com>
2025-02-19 22:20:34 +0530

    initial commit

--- api/Index.js
+++ api/Index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// mongoose.connect("mongodb+srv://m48891981:Riya@mern.sdruk.mongodb.net/");
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error:", err);
+  });
 
 const app = express();
 const PORT = 3000;
